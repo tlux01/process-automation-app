55824873841c7f7c703f34b81f7d55fdf9967bc3
Tyler Lux <tlux@bu.edu>
2022-03-14 08:51:29 -0400

    design work on workflows

--- src/components/stepSequence/StepSequenceItem.tsx
+++ src/components/stepSequence/StepSequenceItem.tsx
@@ -4,16 +4,17 @@ import {
   sequenceNumberStyle,
 } from "../../styles/sequenceStyles";
 import StepAccordion from "../steps/StepAccordion";
+import StepEditor from "../steps/StepEditor";
 
 function StepSequenceItem(props: { stepSequence: StepSequence }) {
   return (
     <>
-      <div className="flex before:content">
+      <div className="flex">
         <div className="flex justify-center align-middle m-2 w-10 h-10 bg-blue-600 rounded">
           <p className="text-white m-auto">{props.stepSequence.stepNumber}</p>
         </div>
-        <div className="align-self-center me-2 h-48">
-          <StepAccordion step={props.stepSequence.step!} />
+        <div className="flex-grow align-self-center me-2 h-48">
+          <StepEditor step={props.stepSequence.step!} />
         </div>
       </div>
     </>
--- src/components/workflows/WorkflowEditor.tsx
+++ src/components/workflows/WorkflowEditor.tsx
@@ -1,25 +1,35 @@
-import { Workflow } from "../../API";
+import { Step, StepSequence, Workflow } from "../../API";
 import StepSequenceItem from "../stepSequence/StepSequenceItem";
 
 function WorkflowEditor(props: { workflow: Workflow }) {
-  const stepSequenceList = props.workflow?.steps?.items.map((stepItem) => {
-    return (
-      <div>
-        <StepSequenceItem stepSequence={stepItem!} />
-      </div>
-    );
-  });
+  const stepSequenceList = (props.workflow?.steps?.items as StepSequence[])
+    .sort((a: StepSequence, b: StepSequence) => {
+      if (a.stepNumber! > b.stepNumber!) {
+        return 1;
+      } else {
+        return -1;
+      }
+    })
+    .map((stepItem) => {
+      return (
+        <>
+          <StepSequenceItem stepSequence={stepItem!} />
+        </>
+      );
+    });
   return (
     <div className="p-3 mb-1 me-1 rounded-r-md rounded-b-md bg-white flex flex-col h-full">
-      <div className="shadow-lg rounded-md flex-grow flex flex-col">
-        <div className="flex-grow flex flex-col">
+      <div className="shadow-lg rounded-md flex-grow flex flex-col overflow-y-auto">
+        <div className="flex-grow flex flex-col overflow-y-auto">
           <div className="flex flex-col bg-gray-100">
             <p className="m-0 me-2 text-sm font-thin">Workflow</p>
             <p className="relative font-medium -top-2 text-2xl m-0">
               {props.workflow.name}
             </p>
           </div>
-          <div className="flex flex-col-reverse">{stepSequenceList}</div>
+          <div className="flex flex-grow flex-col overflow-y-auto">
+            {stepSequenceList}
+          </div>
         </div>
       </div>
     </div>
